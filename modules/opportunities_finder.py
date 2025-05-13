2e66b8648527903b2c445f9267ff052f36bfcbc3
Naveen R <124512345+naveen3830@users.noreply.github.com>
2025-05-13 11:31:24 +0530

    Update opportunities_finder.py

--- modules/opportunities_finder.py
+++ modules/opportunities_finder.py
@@ -152,8 +152,8 @@ def process_url(url, keyword, target_url):
         return None
     try:
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'User-Agent': ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36"
+            )
         }
         response = requests.get(url, headers=headers, timeout=15, verify=False)
         response.raise_for_status()
